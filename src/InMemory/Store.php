8c2f1ffda90e7d868415367a5d894e7dc8d3cd7c
Oskar Stark <oskarstark@googlemail.com>
2025-12-07 23:09:52 +0100

    Move InMemory store from Bridge to store root
    The InMemory store is not a bridge to an external service, so it belongs
    in the main store namespace rather than the Bridge namespace.
    
    - Move src/store/src/Bridge/InMemory to src/store/src/InMemory
    - Move src/store/tests/Bridge/InMemory to src/store/tests/InMemory
    - Update namespace from Symfony\AI\Store\Bridge\InMemory to Symfony\AI\Store\InMemory
    - Update all references in ai-bundle and examples

---
+++ src/InMemory/Store.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Store\InMemory;
+
+use Symfony\AI\Platform\Vector\Vector;
+use Symfony\AI\Store\Distance\DistanceCalculator;
+use Symfony\AI\Store\Document\VectorDocument;
+use Symfony\AI\Store\Exception\InvalidArgumentException;
+use Symfony\AI\Store\ManagedStoreInterface;
+use Symfony\AI\Store\StoreInterface;
+
+/**
+ * @author Guillaume Loulier <personal@guillaumeloulier.fr>
+ */
+class Store implements ManagedStoreInterface, StoreInterface
+{
+    /**
+     * @var VectorDocument[]
+     */
+    private array $documents = [];
+
+    public function __construct(
+        private readonly DistanceCalculator $distanceCalculator = new DistanceCalculator(),
+    ) {
+    }
+
+    public function setup(array $options = []): void
+    {
+        if ([] !== $options) {
+            throw new InvalidArgumentException('No supported options.');
+        }
+
+        $this->drop();
+    }
+
+    public function add(VectorDocument ...$documents): void
+    {
+        array_push($this->documents, ...$documents);
+    }
+
+    /**
+     * @param array{
+     *     maxItems?: positive-int,
+     *     filter?: callable(VectorDocument): bool
+     * } $options If maxItems is provided, only the top N results will be returned.
+     *            If filter is provided, only documents matching the filter will be considered.
+     */
+    public function query(Vector $vector, array $options = []): iterable
+    {
+        $documents = $this->documents;
+
+        if (isset($options['filter'])) {
+            $documents = array_values(array_filter($documents, $options['filter']));
+        }
+
+        yield from $this->distanceCalculator->calculate($documents, $vector, $options['maxItems'] ?? null);
+    }
+
+    public function drop(): void
+    {
+        $this->documents = [];
+    }
+}
